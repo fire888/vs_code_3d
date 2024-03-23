6f3206e0c3612f2bba149a0a26946eff961b253e
vasilii <porokh.vs@gmail.com>
2024-03-23 20:30:04 +0300

    second

--- extension.js
+++ extension.js
@@ -10,6 +10,20 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
+	const panel = vscode.window.createWebviewPanel(
+		'aaaa',
+		'bbbb',
+		vscode.ViewColumn,
+		{ enableScripts: true }
+	)
+
+	panel.webview.html = getWebViewComponent()
+	panel.webview.onDidReceiveMessage(message => {
+		if (message.command === 'alert') {
+			vscode.window.showInformationMessage(message.text);
+		}
+	})
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "code3d" is now active!');
@@ -29,6 +43,28 @@ function activate(context) {
 
 // This method is called when your extension is deactivated
 function deactivate() {}
+
+function getWebViewComponent () {
+	return `
+	<html>
+		<head>
+			<script>
+				var vsCode = acquireVsCodeApi();
+				document.addEventListener('DOMContentLoaded', () => {
+					vsCode.postMessage({ command: 'alert', text: 'New MESSAGE' })
+					const n = document.getElementById('hhh')
+					n.style.color = 'yellow'
+				})
+
+
+			</script>
+		</head>
+		<body>
+			<h1 id='hhh'>AAAAA---</h1>
+		</body>
+	</html>`
+}
+
 
 module.exports = {
 	activate,
